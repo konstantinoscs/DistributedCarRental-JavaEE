5eacce4f86ba42cfaeb07e5552fd90197ccbc894
konstantinoscs <kons.ste@gmail.com>
2019-11-12 17:26:08 +0100

    init commit

---
+++ CarRental-client/src/java/client/Main.java
@@ -0,0 +1,22 @@
+package client;
+
+import java.util.Date;
+import java.util.List;
+import javax.naming.InitialContext;
+import rental.CarType;
+import rental.Reservation;
+import rental.ReservationConstraints;
+import session.CarRentalSessionRemote;
+import session.ManagerSessionRemote;
+
+public class Main extends AbstractTestManagement<CarRentalSessionRemote, ManagerSessionRemote> {
+
+    public Main(String scriptFile) {
+        super(scriptFile);
+    }
+
+    public static void main(String[] args) throws Exception {
+        // TODO: use updated manager interface to load cars into companies
+        new Main("trips").run();
+    }
+}
---
+++ CarRental-ejb/src/java/rental/RentalStore.java
@@ -0,0 +1,96 @@
+package rental;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RentalStore {
+
+    private static Map<String, CarRentalCompany> rentals;
+
+    public static CarRentalCompany getRental(String company) {
+        CarRentalCompany out = RentalStore.getRentals().get(company);
+        if (out == null) {
+            throw new IllegalArgumentException("Company doesn't exist!: " + company);
+        }
+        return out;
+    }
+    
+    public static synchronized Map<String, CarRentalCompany> getRentals(){
+        if(rentals == null){
+            rentals = new HashMap<String, CarRentalCompany>();
+            loadRental("hertz.csv");
+            loadRental("dockx.csv");
+        }
+        return rentals;
+    }
+
+    public static void loadRental(String datafile) {
+        try {
+            CrcData data = loadData(datafile);
+            CarRentalCompany company = new CarRentalCompany(data.name, data.regions, data.cars);
+            rentals.put(data.name, company);
+            Logger.getLogger(RentalStore.class.getName()).log(Level.INFO, "Loaded {0} from file {1}", new Object[]{data.name, datafile});
+        } catch (NumberFormatException ex) {
+            Logger.getLogger(RentalStore.class.getName()).log(Level.SEVERE, "bad file", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(RentalStore.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static CrcData loadData(String datafile)
+            throws NumberFormatException, IOException {
+
+        CrcData out = new CrcData();
+        StringTokenizer csvReader;
+        int nextuid = 0;
+       
+        //open file from jar
+        BufferedReader in = new BufferedReader(new InputStreamReader(RentalStore.class.getClassLoader().getResourceAsStream(datafile)));
+        
+        try {
+            while (in.ready()) {
+                String line = in.readLine();
+                
+                if (line.startsWith("#")) {
+                    // comment -> skip					
+                } else if (line.startsWith("-")) {
+                    csvReader = new StringTokenizer(line.substring(1), ",");
+                    out.name = csvReader.nextToken();
+                    out.regions = Arrays.asList(csvReader.nextToken().split(":"));
+                } else {
+                    csvReader = new StringTokenizer(line, ",");
+                    //create new car type from first 5 fields
+                    CarType type = new CarType(csvReader.nextToken(),
+                            Integer.parseInt(csvReader.nextToken()),
+                            Float.parseFloat(csvReader.nextToken()),
+                            Double.parseDouble(csvReader.nextToken()),
+                            Boolean.parseBoolean(csvReader.nextToken()));
+                    //create N new cars with given type, where N is the 5th field
+                    for (int i = Integer.parseInt(csvReader.nextToken()); i > 0; i--) {
+                        out.cars.add(new Car(nextuid++, type));
+                    }        
+                }
+            } 
+        } finally {
+            in.close();
+        }
+
+        return out;
+    }
+    
+    static class CrcData {
+            public List<Car> cars = new LinkedList<Car>();
+            public String name;
+            public List<String> regions =  new LinkedList<String>();
+    }
+}
