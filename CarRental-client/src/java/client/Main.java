d07afdc6029a427a8af615739ee3da74c3d29516
Sofie Landuyt <sofie.landuyt@student.kuleuven.be>
2019-11-12 18:38:31 +0100

    Update Main.java

--- CarRental-client/src/java/client/Main.java
+++ CarRental-client/src/java/client/Main.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import javax.naming.InitialContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import rental.CarType;
 import rental.Reservation;
 import rental.ReservationConstraints;
@@ -11,6 +13,7 @@ import session.CarRentalSessionRemote;
 import session.ManagerSessionRemote;
 
 public class Main extends AbstractTestManagement<CarRentalSessionRemote, ManagerSessionRemote> {
+    
 
     public Main(String scriptFile) {
         super(scriptFile);
